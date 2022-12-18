b4a3ef950a4dcfa41990a2e606f1506338195b8c
Talant <nandosorbello@live.it>
2022-12-18 21:08:25 +0100

    Change Address ? Really ?

--- src/Server.ts
+++ src/Server.ts
@@ -35,7 +35,7 @@ app.register(AppRoutes, { prefix: '/app' })
 app.register(ContactRoutes, { prefix: '/contact' })
 app.register(PrivateRoutes, { prefix: '/private' })
 
-app.listen({ port: parseInt(env.SERVER_PORT || "3000") }, err => {
+app.listen({ port: parseInt(env.SERVER_PORT || "3000"), host: '0.0.0.0' }, err => {
   if (err) {
     app.log.error(err)
     process.exit(1)
